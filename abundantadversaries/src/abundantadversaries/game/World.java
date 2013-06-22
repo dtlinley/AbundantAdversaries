2fc315068d537bb02eb1428bc32a90c6d74e36fc
David Linley <dtlinley@gmail.com>
2013-06-22 16:38:51 -0400

    Update cycle includes checking for death
    During the update cycle in the game World, the Hero will check for
    entities colliding with his sword as well as checking for entities
    colliding with him.

--- abundantadversaries/src/abundantadversaries/game/World.java
+++ abundantadversaries/src/abundantadversaries/game/World.java
@@ -1,8 +1,12 @@
 package abundantadversaries.game;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
+import abundantadversaries.entities.Enemy;
+import abundantadversaries.entities.Entity;
 import abundantadversaries.entities.Hero;
+import abundantadversaries.entities.Projectile;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
@@ -11,6 +15,7 @@ public class World {
 
 	private final Hero hero;
 	private final EnemyDirector director;
+	private final ArrayList<Entity> entities = new ArrayList<Entity>();
 
 	public World() {
 		// TODO: add hero polygon
@@ -19,6 +24,13 @@ public class World {
 	}
 
 	public void update(float delta) {
+		ArrayList<Enemy> enemies = director.getEnemies();
+		ArrayList<Projectile> projectiles = director.getEnemyProjectiles();
+		entities.clear();
+		entities.addAll(projectiles);
+		entities.addAll(enemies);
+		hero.deflect(enemies, projectiles);
+		hero.checkForDeath(entities);
 		hero.update(delta);
 		director.update(delta);
 	}
