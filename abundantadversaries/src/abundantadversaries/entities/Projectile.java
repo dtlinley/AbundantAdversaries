94bb31122d70b022934ab04ab79edaf5092525e7
David Linley <dtlinley@gmail.com>
2013-06-25 19:38:49 -0400

    Add starter images
    Some basic images have been added. They're definitely not staying, but
    it's nice to have some images to work with.

--- abundantadversaries/src/abundantadversaries/entities/Projectile.java
+++ abundantadversaries/src/abundantadversaries/entities/Projectile.java
@@ -2,22 +2,27 @@ package abundantadversaries.entities;
 
 import java.util.LinkedHashMap;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
 
-public abstract class Projectile extends Mob {
+public class Projectile extends Mob {
 
 	private boolean teamSwitch = false;
 
-	protected Projectile(Polygon bounds) {
+	protected TextureRegion image;
+
+	public Projectile(Polygon bounds) {
 		super(bounds);
+		this.image = new TextureRegion(new Texture("bullet.png"));
 	}
 
 	@Override
 	public LinkedHashMap<TextureRegion, Vector2> getRenderables() {
-		// TODO Auto-generated method stub
-		return null;
+		LinkedHashMap<TextureRegion, Vector2> map = new LinkedHashMap<TextureRegion, Vector2>();
+		map.put(image, getPosition());
+		return map;
 	}
 
 	public void deflect() {
